a0ebd26dbbcd773f85f137be56534a9e7643a872
Nate Goldsborough <nathen418@playantares.com>
2023-11-20 13:37:46 -0600

    changes

---
+++ main.py
@@ -0,0 +1,38 @@
+import rsa
+
+publicKey, privateKey = rsa.newkeys(512)
+
+
+def encrypt():
+    messageToEncrypt = input("Enter a message to encrypt: ") 
+    encoded = rsa.encrypt(messageToEncrypt.encode(), publicKey)
+    print("Encryption key has been downloaded to 'private.pem")
+    # write private key to file "private.pem"
+    with open('private.pem', mode='wb') as privatefile:
+        privatefile.write(privateKey.save_pkcs1())
+    # write encrypted text to file "encrypted.txt"
+    with open('encrypted.enc', mode='wb') as encryptedfile:
+        encryptedfile.write(encoded)
+    print("encrypted string has been writen to 'encrypted.txt'")
+
+def decrypt():
+    messageToDecrypt = input("Enter a filename to decrypt: ")
+    with open(messageToDecrypt, mode='rb') as encryptedfile:
+        messageToDecrypt = encryptedfile.read()
+    with open('private.pem', mode='rb') as privatefile:
+        keydata = privatefile.read()
+    privkey = rsa.PrivateKey.load_pkcs1(keydata)
+    decMessage = rsa.decrypt(messageToDecrypt, privkey).decode()
+    print("decrypted string: ", decMessage)
+
+
+def main():
+    choice = input("Would you like to encrypt or decrypt? ")
+    
+    if (choice.lower() == "encrypt"):
+        encrypt()
+    elif (choice.lower() == "decrypt"):
+        decrypt()
+    
+if (__name__ == "__main__"):
+    main()
